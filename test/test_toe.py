fdfe8531030029697ea46ba6201dd17a8296fa00
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-11-15 19:28:29 +0100

    test structure

--- test/test_toe.py
+++ test/test_toe.py
@@ -1,7 +1,7 @@
 import unittest
 import xml
 
-from src.toes import Toe, render_toe
+from src.toes import Toe, render_toe, Variable_Scope
 
 class ToeTest(unittest.TestCase):
 	
@@ -10,7 +10,44 @@ class ToeTest(unittest.TestCase):
 		self.assertNotEqual(toe.new_tree, None)
 		self.assertEqual(type(toe.new_tree), xml.dom.minidom.Document)
 
-		#render_toe(template="test.html", path_to_templates=os.path.join(os.getcwd(), 'src', 'cms', 'app', 'templates'), title="Ahoy", description="I've got this", enabled=True, lang="cs", way_out=[1,2,3])
+	def test_subtree(self):
+		pass
 
-#if __name__ == '__main__':
-	#unittest.main()
+	def test_toe_import_tag(self):
+		pass
+
+	def test_toe_assign_tag(self):
+		pass
+
+	def test_toe_create_tag(self):
+		pass
+
+	def test_toe_modify_tag(self):
+		pass
+
+	def test_process_condition(self):
+		pass
+
+	def test_toe_if_attr(self):
+		pass
+
+	def test_toe_for_attr(self):
+		pass
+
+	def test_toe_while_attr(self):
+		pass
+
+	def test_variable_scope(self):
+		pass
+
+	def test_find_in_scope(self):
+		pass
+
+	def test_find_in_current_scope(self):
+		pass
+
+	def test_is_variable_in_scope(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
